521d9cf193031c3ebbbfb24c7e7677be6cf3bcf8
sebasgaspar <sebasgaspar2009@hotmail.com>
2020-12-06 14:05:25 -0500

    make it better4

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-
+let port = process.env.PORT || 3000
 
 
 // App de Express
 const app = express();
 
 
-//Lectura y parseo del body
-app.use(express.json());
+// //Lectura y parseo del body
+// app.use(express.json());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+//     next();
+// });
 
-// Node Server
-const server = require('http').createServer(app);
+// // Node Server
+// const server = require('http').createServer(app);
 
 
 // Path público
@@ -28,6 +28,9 @@ const publicPath = path.resolve(__dirname, 'public');
 app.use(express.static(publicPath));
 
 //Mis Rutas
+app.use('/', (req, res) => {
+    res.send('Hello world');
+});
 app.use('/api/client', require('./routes/clients'));
 app.use('/api/empresa', require('./routes/empresa'));
 app.use('/api/user', require('./routes/users'));
@@ -35,10 +38,10 @@ app.use('/api/vehicle', require('./routes/vehicle'));
 app.use('/api/pos', require('./routes/pos'));
 
 
-server.listen(process.env.PORT, (err) => {
+app.listen(port, (err) => {
 
     if (err) throw new Error(err);
 
-    console.log('Servidor corriendo en puerto', process.env.PORT);
+    console.log('Servidor corriendo en puerto', port);
 
 });
